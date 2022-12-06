0bd255c476fb7adf06dd4fdbc1655307b087b6b1
turana1998 <turana221298@gmail.com>
2022-12-06 22:56:52 +0400

    test

--- admin/dark/port-kat-list.php
+++ admin/dark/port-kat-list.php
@@ -21,7 +21,7 @@ require_once "../settings/code/port-kat-delete.php" ?>
                             <div class="card my-4">
                                 <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                                     <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
-                                        <h6 class="text-white text-capitalize ps-3">Portfolio Category List</h6>
+                                        <h6 class="text-white text-capitalize ps-3">Portfolio Catdddegory List</h6>
                                     </div>
                                 </div>
                                 <div class="card-body px-0 pb-2">
